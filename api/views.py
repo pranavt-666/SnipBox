5653a0bf082858cac7179e61dd715f6a712b6c64
Pranav T <pranavt666@gmail.com>
2024-06-16 16:18:13 +0530

    Completed the TagView which will list the tags and retrieve the snippets related to the tags

--- api/views.py
+++ api/views.py
@@ -14,4 +14,20 @@ class TagView(ViewSet):
         queryset = models.Tag.objects.all()
         serializer = serializers.TagSerializer(queryset, many=True)
         return Response(data=serializer.data)
+
+    def retrieve(self, request, *args, **kwargs):
+        tag_id = kwargs.get('pk')
+        tag = models.Tag.objects.get(id=tag_id)
+        snippet = models.Snippet.objects.filter(tag=tag)
+        serializer = serializers.SnippetSerializer(snippet, many=True)
+        return Response(data=serializer.data)
+
+
+class SnippetModelView(ModelViewSet):
+
+    serializer_class = serializers.SnippetSerializer
+    queryset = models.Snippet.objects.all() 
+
+    # def create(self, )
+        
     
