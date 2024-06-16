0604fb75b8e7cd11518c9c060fa22273559b3bbb
Pranav T <pranavt666@gmail.com>
2024-06-16 15:24:20 +0530

    Created the models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Tag(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.title
+class Snippet(models.Model):
+    title = models.CharField(max_length=255)
+    note = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+    created_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
