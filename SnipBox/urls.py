ec8e28807d843c246465ee1a35316ab0a62b2a75
Pranav T <pranavt666@gmail.com>
2024-06-16 15:52:47 +0530

    Imported the default router and created the 'router' instance and linked to the tag view

--- SnipBox/urls.py
+++ SnipBox/urls.py
@@ -16,13 +16,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from api import views
 
+
+router = DefaultRouter()
+
+router.register('tags', views.TagView, basename='tags')
+# router.register('')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-]
+]+router.urls
--- api/views.py
+++ api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from api import models
+from api import serializers
+from rest_framework.response import Response
+from rest_framework.viewsets import ViewSet, ModelViewSet
+
 
 # Create your views here.
+
+
+class TagView(ViewSet):
+    
+    def list(self, request, *args, **kwargs):
+        queryset = models.Tag.objects.all()
+        serializer = serializers.TagSerializer(queryset, many=True)
+        return Response(data=serializer.data)
+    
